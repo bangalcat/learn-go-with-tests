ca7823f9ce4278cd6b50fa6a4b32d6940308a735
donghyun <semaj9310@gmail.com>
2021-05-28 23:42:45 +0900

    hello - spanish, french

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,27 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name + "!"
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name + "!"
 	}
 	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", "English"))
 }
