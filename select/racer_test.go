53b47b6a4c34cdea6636a11a5ea5161abfa16eaa
sunriseslowy <semaj9310@gmail.com>
2021-07-31 02:24:34 +0900

    select: racer - final

--- select/racer_test.go
+++ select/racer_test.go
@@ -7,22 +7,49 @@ import (
 	"time"
 )
 
+/*
+	Wraping up
+	* select
+	  * Helps you wait on multiple channels.
+ 	  * Sometimes you'll want to include time.
+		  After in one of your cases to prevent your system blocking forever.
+
+	* httptest
+		* A convenient way of creating test servers so you can have reliable and controllable tests.
+		* Using the same interfaces as the "real" net/http servers which is consistent and less for you to learn.
+*/
+
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("compares speeds of servers, returning the url of the faster", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(20 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected and error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
